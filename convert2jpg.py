b3d9a85746b02a4297742c4d5a60fd2fecc3e984
victd <55512729+victd@users.noreply.github.com>
2023-09-18 10:24:15 -0400

    Update convert2jpg.py

--- convert2jpg.py
+++ convert2jpg.py
@@ -9,7 +9,7 @@ from PIL import Image
 # build the UI UX
 # for the email blast SMTP bounce backs, check the failure rate and bounce reason
 # from the receiving server
-# rinx booking
+# rinx booking, league or drop-in
 
 if len(sys.argv) > 1:
     if os.path.exists(sys.argv[1]):
